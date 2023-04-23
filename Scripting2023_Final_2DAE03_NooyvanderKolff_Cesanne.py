8154a83a999bf6ec15115a30cc67f9d1bc8d740d
ChapriGit <45454593+ChapriGit@users.noreply.github.com>
2023-04-23 18:15:58 +0200

    Part 1 of Connection layout ~

--- Scripting2023_Final_2DAE03_NooyvanderKolff_Cesanne.py
+++ Scripting2023_Final_2DAE03_NooyvanderKolff_Cesanne.py
@@ -40,6 +40,7 @@ P4 For Maya: Automatic adding and checking out of Perforce Maya files from withi
 
 from abc import ABC, abstractmethod
 from enum import Enum
+from P4 import P4, P4Exception
 from maya import cmds
 
 
@@ -88,10 +89,8 @@ class Connector(P4MayaModule):
         super().__init__(master_layout)
         self.__pref_handler = pref_handler
 
-        self.__set_p4()
-
     def set_handler(self, handler):
-        super().set_handler(handler)
+        self._handler = handler
         self.__set_p4()
 
     def __connect(self):
@@ -107,13 +106,54 @@ class Connector(P4MayaModule):
         pass
 
     def __set_p4(self):
-        pass
+        client = ""
+        self._handler.p4.client = client
 
     def __update_prefs(self):
         pass
 
     def _create_ui(self, master_layout):
         self._ui = cmds.columnLayout(adj=True, p=master_layout)
+        form = cmds.formLayout()
+        height = 20
+        server_label = cmds.text(l="Server: ", h=height)
+        self.__port = cmds.textField(h=height)
+        user_label = cmds.text(l="User: ", h=height)
+        self.__user = cmds.textField(h=height)
+        wsp_label = cmds.text(l="Workspace: ", h=height)
+        self.__workspace = cmds.textField(h=height)
+
+        margin_side = 40
+        margin_middle = 10
+        margin_top = 5
+        padding_top = 20
+        cmds.formLayout(form, e=True, af={(server_label, "left", margin_side), (self.__port, "right", margin_side),
+                                          (user_label, "left", margin_side), (self.__user, "right", margin_side),
+                                          (wsp_label, "left", margin_side), (self.__workspace, "right", margin_side),
+                                          (server_label, "top", padding_top), (self.__port, "top", padding_top)},
+                        ac={(self.__port, "left", margin_middle, server_label),
+                            (self.__user, "left", margin_middle, user_label),
+                            (self.__workspace, "left", margin_middle, wsp_label),
+                            (self.__user, "top", margin_top, self.__port),
+                            (self.__workspace, "top", margin_top, self.__user),
+                            (user_label, "top", margin_top, server_label),
+                            (wsp_label, "top", margin_top, user_label)})
+
+        available_wsp_label = cmds.text(l="Available workspaces", align="left", h=height)
+        wsp_menu = cmds.optionMenu(h=height)
+        cmds.menuItem(label='')
+        cmds.menuItem(label='Workspace 01')
+        cmds.menuItem(label='Workspace 02')
+
+        buttons = cmds.rowLayout(nc=2)
+        cmds.button(l="Connect to P4", bgc=[0.4, 0.5, 0.8])
+        cmds.button(l="Disconnect")
+
+        cmds.formLayout(form, e=True, af={(available_wsp_label, "left", margin_side), (wsp_menu, "right", margin_side),
+                                          (wsp_menu, "left", margin_side*2), (buttons, "right", margin_side),
+                                          (buttons, "bottom", padding_top)},
+                        ac={(available_wsp_label, "top", margin_top, self.__workspace),
+                            (wsp_menu, "top", margin_top, available_wsp_label), (buttons, "top", margin_top, wsp_menu)})
 
     def get_pretty_name(self):
         return "Connect"
@@ -157,7 +197,7 @@ class Rollback(P4MayaModule):
         self._ui = cmds.columnLayout(adj=True, p=master_layout)
 
     def get_pretty_name(self):
-        return "Rollback"
+        return "File History"
 
 
 class CustomSave(P4MayaModule):
@@ -205,10 +245,9 @@ class P4Bar(object):
         self.__create_ui()
         self.__create_log_window()
 
-        self.__log_test()
-
     def set_handler(self, handler):
         self.__handler = handler
+        cmds.iconTextButton(self.__connected_icon, e=True, c=self.__handler.open_window)
 
     def set_connected(self, connected: bool):
         if connected:
@@ -237,7 +276,7 @@ class P4Bar(object):
         cmds.menuItem(l="See Changelist")
         cmds.menuItem(l="File History")
         cmds.menuItem(l="Checks")
-        self.__connected_icon = cmds.iconTextButton(style="iconOnly", i="confirm.png", h=18, w=18)
+        self.__connected_icon = cmds.iconTextButton(style="iconOnly", i="confirm.png", h=18, w=18,)
         self.__connected_text = cmds.text(l="Connected")
 
         log = cmds.rowLayout(nc=3, p=self.__ui)
@@ -283,20 +322,17 @@ class P4MayaControl:
     Base class of P4 for Maya
     """
     def __init__(self, window, bar):
-        self.p4 = None
+        self.p4 = P4()
         self.window = window
         self.bar = bar
 
     def open_window(self):
-        pass
+        cmds.showWindow(self.window)
 
     def change_connection(self, p4):
         pass
 
     def send_to_log(self, log_message, msg_type):
-        pass
-
-    def set_p4(self):
         pass
 
 
@@ -337,7 +373,7 @@ class P4MayaFactory:
     @classmethod
     def __create_window(cls):
         # window = cmds.window("P4MayaWindow", l="P4 Settings and Actions")
-        window = cmds.window(title="P4 Settings and Actions", width=400, height=500)
+        window = cmds.window(title="P4 Settings and Actions", width=400, height=500, ret=True)
         master_layout = cmds.formLayout()
         tabs_layout = cmds.tabLayout(p=master_layout)
         cmds.formLayout(master_layout, e=True, af=[(tabs_layout, "top", 0),
